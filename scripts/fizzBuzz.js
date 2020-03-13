2539aa9c2c3364ea00f46b33f5d8b19751f08ea8
jakejamesreid <jakejamesreid@gmail.com>
2020-03-13 16:16:16 +0000

    Project Completed

--- scripts/fizzBuzz.js
+++ scripts/fizzBuzz.js
@@ -1,5 +1,8 @@
 function fizzBuzz(number){
-    if(number % 5 == 0 && number % 3 == 0){
+    if(typeof(number)!= "number"){
+        return "Sorry, numerical values only";
+    }
+    else if(number % 5 == 0 && number % 3 == 0){
         return "FizzBuzz";
     }
     else if(number % 3 == 0){
--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,7 +1,20 @@
 describe("My FizzBuzz function", function(){
+// A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function.
 
+beforeEach(function() {
+    fizzbuzz = new fizzBuzz();
+});
 
-    describe("Checks FizzBuzz", function(){
+    describe("Returns number, fizz, buzz or fizzbuzz", function(){
+
+        // Specs are defined by calling the global Jasmine function it
+        it("should exist", function() {
+            // Expectations are built with the function expect which takes a value, called the actual.
+            // Each matcher implements a boolean comparison between the actual value and the expected value.
+            // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
+            expect(fizzBuzz).toBeDefined();
+        });
+
         it("should return 'FizzBuzz", function(){
             expect(fizzBuzz(15)).toBe("FizzBuzz");
         });
@@ -10,6 +23,13 @@ describe("My FizzBuzz function", function(){
         });
         it("should return 'Buzz", function(){
             expect(fizzBuzz(10)).toBe("Buzz");
+        });
+        it("should return 2 when called as fizzBuzz(2)", function() {
+            expect(fizzBuzz(2)).toBe(2);
+        });
+
+        it("should be unable to return an answer when called with a string", function(){
+            expect(fizzBuzz("Hi")).toBe("Sorry, numerical values only");
         });
     });
 
