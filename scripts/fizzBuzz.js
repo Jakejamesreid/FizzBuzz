32dd47ff6474f3ecd0283432b2868b1e48b42de0
jakejamesreid <jakejamesreid@gmail.com>
2020-03-13 16:08:25 +0000

    Basic functionality met

--- scripts/fizzBuzz.js
+++ scripts/fizzBuzz.js
@@ -1,5 +1,15 @@
 function fizzBuzz(number){
-    if(number % 5 == 0 || number % 3 == 0){
+    if(number % 5 == 0 && number % 3 == 0){
         return "FizzBuzz";
     }
+    else if(number % 3 == 0){
+        return "Fizz";
+    }
+    else if(number % 5 == 0){
+        return "Buzz";
+    }
+    else{
+        return number;
+    }
+
 }
--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -5,6 +5,12 @@ describe("My FizzBuzz function", function(){
         it("should return 'FizzBuzz", function(){
             expect(fizzBuzz(15)).toBe("FizzBuzz");
         });
+        it("should return 'Fizz", function(){
+            expect(fizzBuzz(6)).toBe("Fizz");
+        });
+        it("should return 'Buzz", function(){
+            expect(fizzBuzz(10)).toBe("Buzz");
+        });
     });
 
 
